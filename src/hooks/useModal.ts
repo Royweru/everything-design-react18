0189bb3cf2bf5dba6ff3a02e8fccac6706adeae1
Royweru <royezi17@gmail.com>
2024-05-14 13:24:50 +0300

    new file:   src/hooks/useModal.ts

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,26 @@
+import {create} from "zustand"
+
+interface Product{
+    id?:number,
+    price?:number,
+    name?:string,
+    description?:string,
+    thumbnail?:string,
+    images?:string[]
+}
+
+interface storeModal{
+    isOpen:boolean,
+    onOpen:(data:Product)=>void,
+    onClose:()=>void,
+    data:Product|null
+}
+
+export const useModal =  create<storeModal>((set)=>(
+    {
+        isOpen:false,
+        onOpen:(data={})=>set({data,isOpen:true}),
+        onClose:()=>set({isOpen:false}),
+        data:{}
+    }
+))
