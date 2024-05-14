ec1d6e8e8fb29ed197b621bf7e96ed19f08676a1
Royweru <royezi17@gmail.com>
2024-05-14 16:45:01 +0300

    modified:   src/components/shared/modal-socials.tsx

--- src/components/shared/modal-socials.tsx
+++ src/components/shared/modal-socials.tsx
@@ -3,9 +3,16 @@ import { FaInstagram, FaTwitter,FaWhatsapp } from 'react-icons/fa';
 export const ModalSocials = () => {
   return (
     <div className=" w-full flex justify-center items-center space-x-4 font-bold text-color-4">
-        <FaInstagram  className=' w-5 h-5'/>
-        <FaTwitter  className=' w-5 h-5'/>
+        <a href="https://wa.me/+254759355344" target='_blank'>
         <FaWhatsapp  className=' w-5 h-5'/>
+        </a>
+        <a href="https://www.instagram.com/_g.weru/" target='_blank'>
+        <FaInstagram  className=' w-5 h-5'/>
+        </a>
+        <a href="https://wa.me/+254759355344" target='_blank'>
+        <FaTwitter className=' w-5 h-5'/>
+        </a>
+       
     </div>
   )
 }
