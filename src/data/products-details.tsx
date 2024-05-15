8a25c5d9b52d33c2d77d2be1894af9ed33d1f0d3
Royweru <royezi17@gmail.com>
2024-05-15 15:01:03 +0300

    new file:   public/images/hoodies/bh1.jpg 	new file:   public/images/hoodies/bh2.jpg 	new file:   public/images/hoodies/bh3.jpg 	modified:   src/data/products-details.tsx

--- src/data/products-details.tsx
+++ src/data/products-details.tsx
@@ -109,15 +109,12 @@ export const brandedTshirts:{id:number,price:number,name:string,description:stri
 export const brandedHoodies:{id:number,name:string,price:number,description:string,thumbnail:string,images:string[]}[]=[
     {
         id:1,
-        name:"Branded Mugs",
-        price:550,
+        name:"Make money not....",
+        price:2500,
         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        thumbnail:"https://img.freepik.com/free-photo/inspirational-quote-written-mug_23-2149261735.jpg?t=st=1715585905~exp=1715589505~hmac=3178de1027b125fdcce96bb423244f7edca047f6c24c9812badb895a20f5abd6&w=900",
+        thumbnail:"/images/hoodies/bh2.jpg",
         images:[
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-             "https://img.freepik.com/premium-vector/creative-abstract-background_17005-111.jpg?w=740",
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+             "/images/hoodies/bh2.jpg",
         ]
     },
     {
