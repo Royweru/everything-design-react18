598d5018c897c590aa62f9c4f507cc65390cfbad
Royweru <royezi17@gmail.com>
2024-05-15 11:47:18 +0300

    new file:   public/images/mugs/5.jpg 	new file:   public/images/mugs/6.jpg 	new file:   public/images/mugs/7.jpg 	new file:   public/images/t-shirts/bt10.jpg 	new file:   public/images/t-shirts/bt11.jpg 	new file:   public/images/t-shirts/bt12.jpg 	new file:   public/images/t-shirts/bt2.jpg 	new file:   public/images/t-shirts/bt3.jpg 	new file:   public/images/t-shirts/bt4.jpg 	new file:   public/images/t-shirts/bt5.jpg 	new file:   public/images/t-shirts/bt6.jpg 	new file:   public/images/t-shirts/bt7.jpg 	new file:   public/images/t-shirts/bt8.jpg 	new file:   public/images/t-shirts/bt9.jpg 	modified:   src/data/products-details.tsx

--- src/data/products-details.tsx
+++ src/data/products-details.tsx
@@ -3,7 +3,7 @@ export const brandedMugs:{id:number,price:number,name:string,description:string,
         id:1,
         name:"Mama mug",
         price:650,
-        description:" Get ",
+        description:`Get this charming "Mama" mug and make your mornings brighter. Perfect for coffee, tea, or any favorite beverage, this mug is a lovely way to show appreciation for the special mama in your life. Crafted from high-quality ceramic, it features a comfortable handle and a stylish design that will stand out in any kitchen. Whether as a gift or a treat for yourself, this mug is a wonderful way to celebrate motherhood and start the day with a smile.`,
         thumbnail:"https://img.freepik.com/free-photo/inspirational-quote-written-mug_23-2149261735.jpg?t=st=1715585905~exp=1715589505~hmac=3178de1027b125fdcce96bb423244f7edca047f6c24c9812badb895a20f5abd6&w=900",
         images:[
              "/images/mugs/mama.jpg"     
@@ -14,7 +14,7 @@ export const brandedMugs:{id:number,price:number,name:string,description:string,
         id:2,
         name:"Coffe beans",
         price:650,
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        description:"Experience the rich and bold flavors of our premium Coffee Beans. Sourced from the finest coffee-growing regions, these beans are expertly roasted to perfection, ensuring a smooth and aromatic cup every time. Whether you prefer a strong espresso or a mellow drip coffee, our beans deliver a satisfying and flavorful experience. Ideal for coffee enthusiasts who appreciate quality and freshness, these beans are perfect for brewing at home or in the office. Elevate your coffee routine with the exceptional taste of our Coffee Beans.",
         thumbnail:"/images/mugs/4.jpg",
         images:[
             "/images/mugs/4.jpg",
@@ -47,7 +47,7 @@ export const brandedTshirts:{id:number,price:number,name:string,description:stri
     {
         id:1,
         name:"Keep calm..",
-        price:650,
+        price:1000,
         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         thumbnail:"https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C61VPSN2xV-L.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UY1000_.png",
         images:[
@@ -58,54 +58,49 @@ export const brandedTshirts:{id:number,price:number,name:string,description:stri
     },
     {
         id:2,
-        name:"Savannah",
-        price:650,
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        thumbnail:"https://img.freepik.com/free-photo/inspirational-quote-written-mug_23-2149261735.jpg?t=st=1715585905~exp=1715589505~hmac=3178de1027b125fdcce96bb423244f7edca047f6c24c9812badb895a20f5abd6&w=900",
+        name:"2Pac Shakur",
+        price:1000,
+        description:"Get your t-shirt branded with your favorite celebrity. This shirt features a design inspired by the legendary 2Pac Shakur. Made from high-quality materials, it offers both comfort and style. Perfect for fans of 2Pac and those who want to celebrate the legacy of an iconic artist. Make a bold statement with this exclusive piece.It may be an artist of your choice, Drake , Kendick, ",
+        thumbnail:"/images/t-shirts/bt6.jpg",
         images:[
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-             "https://img.freepik.com/premium-vector/creative-abstract-background_17005-111.jpg?w=740",
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+             "/images/t-shirts/bt6.jpg",
+             "/images/t-shirts/bt4.jpg",
+             "/images/t-shirts/bt2.jpg"
         ]
     },
     {
         id:3,
-        name:"Nairobi skyline",
-        price:650,
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        thumbnail:"https://img.freepik.com/free-photo/inspirational-quote-written-mug_23-2149261735.jpg?t=st=1715585905~exp=1715589505~hmac=3178de1027b125fdcce96bb423244f7edca047f6c24c9812badb895a20f5abd6&w=900",
+        name:"Boondocks",
+        price:1200,
+        description:"Get your t-shirt branded with the iconic Boondocks design. This premium-quality shirt offers comfort and style, perfect for fans of the acclaimed series. Wear your fandom proudly and make a statement with this exclusive piece.",
+        thumbnail:"/images/t-shirts/bt2.jpg",
         images:[
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-             "https://img.freepik.com/premium-vector/creative-abstract-background_17005-111.jpg?w=740",
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+             "/images/t-shirts/bt8.jpg",
+             "/images/t-shirts/bt9.jpg",
+
         ]
     },
     {
         id:4,
-        name:"Savannah",
-        price:650,
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        thumbnail:"https://img.freepik.com/free-photo/inspirational-quote-written-mug_23-2149261735.jpg?t=st=1715585905~exp=1715589505~hmac=3178de1027b125fdcce96bb423244f7edca047f6c24c9812badb895a20f5abd6&w=900",
+        name:"Forgiven",
+        price:950,
+        description:" Get t-shirts with your own custom words of your choice. Personalize your style with a message that means something to you. Made from high-quality materials, this t-shirt combines comfort and individuality. Perfect for expressing yourself and making a unique statement.",
+        thumbnail:"/images/t-shirts/bt7.jpg",
         images:[
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-             "https://img.freepik.com/premium-vector/creative-abstract-background_17005-111.jpg?w=740",
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+             "/images/t-shirts/bt7.jpg",
+             "/images/t-shirts/bt10.jpg",
+             "/images/t-shirts/bt11.jpg",
+             "/images/t-shirts/bt12.jpg"
         ]
     },
     {
         id:5,
-        name:"White cup",
-        price:650,
-        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        thumbnail:"https://img.freepik.com/free-photo/inspirational-quote-written-mug_23-2149261735.jpg?t=st=1715585905~exp=1715589505~hmac=3178de1027b125fdcce96bb423244f7edca047f6c24c9812badb895a20f5abd6&w=900",
+        name:"Gotha gotha tena...",
+        price:1000,
+        description:`Get a t-shirt with the iconic "Gotha gotha tena" wording. This stylish and comfortable tee is perfect for fans who love to make a statement. Made from high-quality cotton, it offers a soft feel and a great fit for everyday wear. Whether you're out with friends or relaxing at home, this t-shirt is a fantastic addition to your wardrobe. Show off your unique style and embrace the fun with this eye-catching piece!`,
+        thumbnail:"/images/slide3.png",
         images:[
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-             "https://img.freepik.com/premium-vector/creative-abstract-background_17005-111.jpg?w=740",
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-             "https://images.pexels.com/photos/18868628/pexels-photo-18868628/free-photo-of-mugs-with-logo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+             "/images/slide3.png",
         ]
     },
 ]
