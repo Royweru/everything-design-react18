7e947a0cf8e3d1cafcad6de638b522aebd3665e5
Royweru <royezi17@gmail.com>
2024-06-11 13:29:59 +0300

    modified:   src/actions/api.ts

--- src/actions/api.ts
+++ src/actions/api.ts
@@ -5,8 +5,6 @@ const api = axios.create({
   baseURL: "http://127.0.0.1:8000",
 });
 
-console.log(api);
-
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
