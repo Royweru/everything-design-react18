b9539ee2e22a60f2bb4c1e68e800e2a2d3a0ee56
Royweru <royezi17@gmail.com>
2024-06-23 11:32:15 +0300

    modified:   src/components/product-card.tsx

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -16,7 +16,7 @@ export const ProductCard = ({
   return (
     <div
       key={product.name}
-      className="shrink-0 cursor-pointer flex flex-col gap-y-2 overflow-hidden bg-text-dark p-1 rounded-xl"
+      className="shrink-0 cursor-pointer flex flex-col gap-y-2 overflow-hidden bg-accent-light p-1 rounded-xl"
       onClick={() => onOpen(product)}
     >
       <div className=" rounded-md relative">
